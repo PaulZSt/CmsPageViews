eed07f6af58cc3b9b9c2f435b33b5bdaaec5ff99
PaulZSt <38794592+PaulZSt@users.noreply.github.com>
2020-09-12 18:42:44 +0300

    v0.9.2
    v0.9.2

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+
+    'Petrovski_CmsPageViews',
+
+    __DIR__
+
+);
